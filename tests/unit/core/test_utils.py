d4f5801731eaa8db595688d43e3a2e55e0190a20
Lucas <lucassrod@gmail.com>
2022-04-12 12:54:09 -0300

    Split the test_raises_not_found_check_is_inactive_or_deleted

--- tests/unit/core/test_utils.py
+++ tests/unit/core/test_utils.py
@@ -31,16 +31,20 @@ def test_check_is_inactive_or_deleted():
 
 
 @pytest.mark.unit
-def test_raises_not_found_check_is_inactive_or_deleted():
-
+def test_raises_not_found_check_is_inactive():
     data = dict(title="some test", slug="some-test", status=ActivatorModel.INACTIVE_STATUS, is_deleted=False)
     category_inactive = entities.Category(**data)
 
+    with pytest.raises(NotFound):
+        utils.raises_not_found_when_inactive_or_deleted(category_inactive)
+
+
+@pytest.mark.unit
+def test_raises_not_found_check_is_deleted():
     data = dict(title="some test", slug="some-test", is_deleted=True)
     category_deleted = entities.Category(**data)
 
     with pytest.raises(NotFound):
-        utils.raises_not_found_when_inactive_or_deleted(category_inactive)
         utils.raises_not_found_when_inactive_or_deleted(category_deleted)
 
 
