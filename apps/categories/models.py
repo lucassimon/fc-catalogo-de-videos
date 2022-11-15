eaa6c471fafda492806c437b7e3479d5478a0806
Lucas <lucassrod@gmail.com>
2022-11-15 14:51:00 -0300

    Create Signal when create/update a category

--- apps/categories/models.py
+++ apps/categories/models.py
@@ -7,6 +7,12 @@ from apps.core.managers import SoftDeleteAndInactiveManager
 from apps.core.messages import CATEGORY as CATEGORY_TEXT
 from apps.core.messages import CATEGORIES
 
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+
+
+# from main.celery import rabbitmq_producer
+
 
 class Category(
     TitleSlugDescriptionModel,
@@ -25,3 +31,11 @@ class Category(
         ordering = ["created"]
         verbose_name = CATEGORY_TEXT
         verbose_name_plural = CATEGORIES
+
+
+# @receiver(post_save, sender=Category)
+# def category_sync(instance, created, **kwargs):
+#     action = "created" if created else "updated"
+#     routing_key = f"model.category.{action}"
+#     with rabbitmq_producer() as producer:
+#         producer.publish(body={"id": instance.id, "name": instance.name}, routing_key=routing_key, exchange="amq.topic")
